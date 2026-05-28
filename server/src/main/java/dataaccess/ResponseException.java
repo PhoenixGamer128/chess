cc6cb94edbc7b826b87eb7397a9d183c60df8863
Hugo <hugo4zelda@gmail.com>
2026-05-28 11:27:51 -0600

    add http methods to ResponseException

--- server/src/main/java/dataaccess/ResponseException.java
+++ server/src/main/java/dataaccess/ResponseException.java
@@ -2,6 +2,7 @@ package dataaccess;
 
 import com.google.gson.Gson;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ResponseException extends DataAccessException {
@@ -26,9 +27,15 @@ public class ResponseException extends DataAccessException {
     }
 
     public String toJson() {
-        return new Gson().toJson(Map.of("message", getMessage()));
+        return new Gson().toJson(Map.of("message", getMessage(), "status", code));
     }
 
+    public static ResponseException fromJson(String json) {
+        var map = new Gson().fromJson(json, HashMap.class);
+        var status = Code.valueOf(map.get("status").toString());
+        String message = map.get("message").toString();
+        return new ResponseException(status, message);
+    }
 
     public int toHttpStatusCode(ResponseException ex) {
         return switch (ex.code()) {
@@ -37,6 +44,14 @@ public class ResponseException extends DataAccessException {
             case Code.BadRequest -> 400;
             case Code.Unauthorized -> 401;
             case Code.NotFound -> 404;
+        };
+    }
+
+    public Code fromHttpStatusCode(int code) {
+        return switch (code) {
+            case 500 -> Code.DataAccess;
+            case 400 -> Code.BadRequest;
+            default -> throw new IllegalArgumentException("Unknown HTTP status code: " + code);
         };
     }
 }
